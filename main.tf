b0abc956ab72bfc6401200efe3f7a64af2201911
Matar Peles <matarpeles@Matars-MacBook-Pro.local>
2023-05-31 16:34:19 +0300

    Added all required variables

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
 module "port_aws_exporter" {
   source = "github.com/port-labs/port-aws-exporter-module"
 
-  config        = "config.json"  # Update with the actual path to your configuration file
-  bucket_name   = "port-aws-exporter"  # Bucket for saving the config.json.
-                                       # Default: if doesn't exist, create the bucket
-  secret_name   = "port-credentials"  # Secret for saving the Port credentials.
-                                      # Default: if doesn't exist, create the secret
-  function_name = "my-port-exporter-function"  # The Lambda name
-  iam_permissions     = "path/to/iam/roles/file"  # Optional - Path to a file containing the permissions to set for the Lambda.
-                                                  # Default - creating a role with permissions to use
-                                                  # CloudControl / CloudFormation API for all resources
+  config        = var.config  # Update with the actual path to your configuration file
+  bucket_name   = var.bucket_name  # Bucket for saving the config.json.
+                                   # Default: if doesn't exist, create the bucket
+  secret_name   = var.secret_name  # Secret for saving the Port credentials.
+                                  # Default: if doesn't exist, create the secret
+  function_name = var.function_name  # The Lambda name
+  iam_permissions = var.iam_permissions  # Optional - Path to a file containing the permissions to set for the Lambda.
+                                              # Default - creating a role with permissions to use
+                                              # CloudControl / CloudFormation API for all resources
+  create_bucket = var.create_bucket # Optional - flag to determine whether to create a bucket for the configuration file
+
 }
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "config" {
+  type    = string
+  default = "config.json"
+}
+
+variable "bucket_name" {
+  type    = string
+  default = "port-aws-exporter"
+}
+
+variable "secret_name" {
+  type    = string
+  default = "port-credentials"
+}
+
+variable "function_name" {
+  type    = string
+  default = "my-port-exporter-function"
+}
+
+# Optional variables for the module
+variable "iam_permissions" {
+  type    = string
+  default = "path/to/iam/roles/file"
+}
+
+variable "create_bucket" {
+  type        = bool
+  description = "Whether to create the bucket or not"
+  default     = true
+}
+
+variable "schedule_expression" {
+  type        = string
+  description = "The schedule expression for triggering the Lambda function"
+}
